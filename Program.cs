336916da35b445af5967a9cab14104cd64031d19
felipesilva017 <felipemsilva017@gmail.com>
2024-06-15 21:07:09 -0300

    implementando classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "85993470294", modelo: "ms3", imei: "930403", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.ReadKey();
+Console.WriteLine();
+
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "85993472204", modelo: "15 pro", imei: "4343443", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.ReadKey();
